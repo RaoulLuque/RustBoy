b3dd004b7f3cdda75ecba506b9d47a46ce68a011
Raoul Luqué <raoulsluque@gmail.com>
2025-02-20 20:52:44 +0100

    Write code for creating a simple window

---
+++ src/lib.rs
@@ -0,0 +1,44 @@
+#![warn(missing_docs)] // Warn if public items lack documentation
+#![warn(unused_imports)] // Warn about unused imports
+#![warn(unused_variables)] // Warn about unused variables
+#![warn(unreachable_code)] // Warn if there is unreachable code
+#![warn(dead_code)] // Warn about unused functions, structs, etc.
+#![warn(non_snake_case)] // Warn about non-standard naming conventions
+#![warn(rust_2021_compatibility)] // Warn about issues with Rust 2021 edition
+#![warn(clippy::all)] // Enable all Clippy lints
+
+//! This crate provides the methods used to run a Gameboy emulator written in Rust, so it can be run both natively and on the web using WebAssembly.
+
+use winit::{
+    event::*,
+    event_loop::EventLoop,
+    keyboard::{KeyCode, PhysicalKey},
+    window::WindowBuilder,
+};
+
+/// Run the emulator.
+pub fn run() {
+    env_logger::init();
+    let event_loop = EventLoop::new().unwrap();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    event_loop.run(move |event, control_flow| match event {
+        Event::WindowEvent {
+            ref event,
+            window_id,
+        } if window_id == window.id() => match event {
+            WindowEvent::CloseRequested
+            | WindowEvent::KeyboardInput {
+                event:
+                KeyEvent {
+                    state: ElementState::Pressed,
+                    physical_key: PhysicalKey::Code(KeyCode::Escape),
+                    ..
+                },
+                ..
+            } => control_flow.exit(),
+            _ => {}
+        },
+        _ => {}
+    }).expect("Event loop should be able to run");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use rustboy::run;
+
 fn main() {
-    println!("Hello, world!");
+    run();
 }
