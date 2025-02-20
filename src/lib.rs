242acf2ea60050c735653477041262ed69b4461c
Raoul Luqué <raoulsluque@gmail.com>
2025-02-20 22:27:40 +0100

    Setup basic html site to host the WASM code

--- src/lib.rs
+++ src/lib.rs
@@ -47,7 +47,7 @@ pub fn run() {
         web_sys::window()
             .and_then(|win| win.document())
             .and_then(|doc| {
-                let dst = doc.get_element_by_id("wasm-example")?;
+                let dst = doc.get_element_by_id("emulator-body")?;
                 let canvas = web_sys::Element::from(window.canvas()?);
                 dst.append_child(&canvas).ok()?;
                 Some(())
