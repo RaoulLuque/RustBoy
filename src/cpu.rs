9b5ad55fcbc49b9ae88738564e0a2dac6380878e
Raoul Luqué <raoulsluque@gmail.com>
2025-02-25 10:56:35 +0100

    Add scaffolding to support prefixed instructions

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,11 +1,14 @@
 mod instructions;
 mod registers;
 
+use instructions::Instruction;
 use registers::Registers;
 
 /// Struct to represent the CPU.
 /// The CPU has 8 registers, a program counter (PC), a stack pointer (SP), and a memory bus.
 /// For details please refer to [Pan Docs](https://gbdev.io/pandocs/CPU_Registers_and_Flags.html).
+///
+/// For implementations of the CPU instructions please see [instructions].
 pub struct CPU {
     registers: Registers,
     pc: u16,
@@ -13,4 +16,43 @@ pub struct CPU {
     bus: MemoryBus,
 }
 
-struct MemoryBus {}
+impl CPU {
+    /// Reads the next instruction and executes it in the CPU.
+    /// Doing so, the program counter (pc) is updated to point to the address of the next instruction.
+    fn step(&mut self) {
+        let mut instruction_byte = self.bus.read_byte(self.pc);
+
+        // Check if the instruction is a CB instruction (prefix)
+        let prefixed = instruction_byte == 0xCB;
+        if prefixed {
+            instruction_byte = self.bus.read_byte(self.pc + 1);
+        }
+
+        let next_pc = if let Some(instruction) = Instruction::from_byte(instruction_byte, prefixed)
+        {
+            self.execute(instruction)
+        } else {
+            let panic_description = format!(
+                "0x{}{:x}",
+                if prefixed { "CB" } else { "" },
+                instruction_byte
+            );
+            panic!("Invalid instruction found for: {}", panic_description);
+        };
+
+        self.pc = next_pc;
+    }
+}
+
+/// Struct to represent the memory bus.
+/// It is an array that represents the memory of the RustBoy.
+/// 0xFFFF = 65536 is the size of the memory in bytes
+struct MemoryBus {
+    memory: [u8; 0xFFFF],
+}
+
+impl MemoryBus {
+    fn read_byte(&self, address: u16) -> u8 {
+        self.memory[address as usize]
+    }
+}
--- src/cpu/instructions.rs
+++ src/cpu/instructions.rs
@@ -1,7 +1,7 @@
 use super::CPU;
 use crate::cpu::registers::Registers;
 
-enum Instruction {
+pub enum Instruction {
     ADD(ArithmeticTarget),
 }
 
@@ -13,6 +13,51 @@ enum ArithmeticTarget {
     E,
     H,
     L,
+}
+
+impl Instruction {
+    /// Returns the (prefix or non-prefix) instruction corresponding to the given byte. See
+    /// [Interactive CPU Instructions](https://meganesu.github.io/generate-gb-opcodes/)
+    /// for details.
+    ///
+    /// Checks the prefix bools to determine if a prefix instruction should be returned or not.
+    /// That is, the u8 byte should only contain the instruction byte and not include the prefix byte.
+    ///
+    /// Calls [Instruction::from_byte_not_prefixed] or [Instruction::from_byte_prefixed] depending on the prefix bool.
+    pub fn from_byte(byte: u8, prefixed: bool) -> Option<Instruction> {
+        if prefixed {
+            Self::from_byte_prefixed(byte)
+        } else {
+            Self::from_byte_not_prefixed(byte)
+        }
+    }
+
+    /// Returns the prefix instruction corresponding to the given byte. See
+    /// [Interactive CPU Instructions](https://meganesu.github.io/generate-gb-opcodes/)
+    /// for details.
+    pub fn from_byte_prefixed(byte: u8) -> Option<Instruction> {
+        match byte {
+            // TODO: Add more instructions
+            _ => None,
+        }
+    }
+
+    /// Returns the non-prefix instruction corresponding to the given byte. See
+    /// [Interactive CPU Instructions](https://meganesu.github.io/generate-gb-opcodes/)
+    /// for details.
+    pub fn from_byte_not_prefixed(byte: u8) -> Option<Instruction> {
+        match byte {
+            // TODO: Add more instructions
+            0x87 => Some(Instruction::ADD(ArithmeticTarget::A)),
+            0x80 => Some(Instruction::ADD(ArithmeticTarget::B)),
+            0x81 => Some(Instruction::ADD(ArithmeticTarget::C)),
+            0x82 => Some(Instruction::ADD(ArithmeticTarget::D)),
+            0x83 => Some(Instruction::ADD(ArithmeticTarget::E)),
+            0x84 => Some(Instruction::ADD(ArithmeticTarget::H)),
+            0x85 => Some(Instruction::ADD(ArithmeticTarget::L)),
+            _ => None,
+        }
+    }
 }
 
 impl ArithmeticTarget {
@@ -31,13 +76,18 @@ impl ArithmeticTarget {
 }
 
 impl CPU {
-    /// Executes a given instruction on the CPU.
-    fn execute(&mut self, instruction: Instruction) {
+    /// Executes the instruction on the CPU.
+    pub fn execute(&mut self, instruction: Instruction) -> u16 {
         match instruction {
             Instruction::ADD(target) => {
                 let value = target.get_register(&mut self.registers);
                 let new_value = self.add(value);
                 self.registers.a = new_value;
+                self.pc.wrapping_add(1)
+            }
+            _ => {
+                /* TODO: Support more instructions */
+                self.pc
             }
         }
     }
