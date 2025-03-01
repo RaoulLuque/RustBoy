12e97800eeebb9373c8f75274124933f9e610970
Raoul Luqué <raoulsluque@gmail.com>
2025-03-01 20:34:32 +0100

    Change triangle position

--- src/frontend/shader.rs
+++ src/frontend/shader.rs
@@ -7,15 +7,15 @@ pub(super) struct Vertex {
 
 pub(super) const VERTICES: &[Vertex] = &[
     Vertex {
-        position: [0.0, 0.5, 0.0],
+        position: [-1.0, 1.0, 0.0],
         color: [1.0, 0.0, 0.0],
     },
     Vertex {
-        position: [-0.5, -0.5, 0.0],
+        position: [-1.0, -1.0, 0.0],
         color: [0.0, 1.0, 0.0],
     },
     Vertex {
-        position: [0.5, -0.5, 0.0],
+        position: [1.0, -1.0, 0.0],
         color: [0.0, 0.0, 1.0],
     },
 ];
