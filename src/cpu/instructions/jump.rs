f9f3790c90a857500242f46d3d6475f50d08bce3
Raoul Luqué <raoulsluque@gmail.com>
2025-03-03 13:29:50 +0100

    Change standard addition to wrapping add for more clarity

--- src/cpu/instructions/jump.rs
+++ src/cpu/instructions/jump.rs
@@ -40,7 +40,7 @@ impl RustBoy {
             // The Game Boy is little endian so the least significant byte is stored first. However,
             // in the correct order, so we can just patch them together.
             let low_byte = self.read_byte(self.pc.wrapping_add(1)) as u16;
-            let high_byte = self.read_byte(self.pc + 2) as u16;
+            let high_byte = self.read_byte(self.pc.wrapping_add(2)) as u16;
             (high_byte << 8) | low_byte
         } else {
             // If we don't jump we just move to the next instruction.
