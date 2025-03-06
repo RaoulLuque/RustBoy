de69c2668db51d73adcc0a884caacb72f627fdcb
Raoul Luqué <raoulsluque@gmail.com>
2025-03-06 19:32:07 +0100

    Fix RLCA, RRCA, RLA and RRA instructions by setting zero flag to false

--- src/cpu/instructions/rlca_rrca_rla_and_rra.rs
+++ src/cpu/instructions/rlca_rrca_rla_and_rra.rs
@@ -2,7 +2,8 @@ use super::SixteenBitInstructionTarget;
 use crate::RustBoy;
 
 impl RustBoy {
-    /// Handles the RLCA instruction.
+    /// Handles the RLCA instruction. In comparison to the RLC instruction, the RLCA instruction
+    /// sets the zero flag to false.
     ///
     /// The RLCA instruction takes 1 cycle.
     pub fn handle_rlca_instruction(&mut self) -> u16 {
@@ -10,10 +11,12 @@ impl RustBoy {
         let new_value = self.rlc(value);
         self.registers.a = new_value;
         self.increment_cycle_counter(1);
+        self.registers.f.zero = false;
         self.pc.wrapping_add(1)
     }
 
-    /// Handles the RRCA instruction.
+    /// Handles the RRCA instruction. In comparison to the RRC instruction, the RRCA instruction
+    /// sets the zero flag to false.
     ///
     /// The RRCA instruction takes 1 cycle.
     pub fn handle_rrca_instruction(&mut self) -> u16 {
@@ -21,10 +24,12 @@ impl RustBoy {
         let new_value = self.rrc(value);
         self.registers.a = new_value;
         self.increment_cycle_counter(1);
+        self.registers.f.zero = false;
         self.pc.wrapping_add(1)
     }
 
-    /// Handles the RLA instruction.
+    /// Handles the RLA instruction. In comparison to the RL instruction, the RLA instruction
+    /// sets the zero flag to false.
     ///
     /// The RLA instruction takes 1 cycle.
     pub fn handle_rla_instruction(&mut self) -> u16 {
@@ -32,10 +37,12 @@ impl RustBoy {
         let new_value = self.rl(value);
         self.registers.a = new_value;
         self.increment_cycle_counter(1);
+        self.registers.f.zero = false;
         self.pc.wrapping_add(1)
     }
 
-    /// Handles the RRA instruction.
+    /// Handles the RRA instruction. In comparison to the RR instruction, the RRA instruction
+    /// sets the zero flag to false.
     ///
     /// The RRA instruction takes 1 cycle.
     pub fn handle_rra_instruction(&mut self) -> u16 {
@@ -43,6 +50,7 @@ impl RustBoy {
         let new_value = self.rr(value);
         self.registers.a = new_value;
         self.increment_cycle_counter(1);
+        self.registers.f.zero = false;
         self.pc.wrapping_add(1)
     }
 }
