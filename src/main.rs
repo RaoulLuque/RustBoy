450ce923e5f89741635c0a0e3a05e047c80a9b61
Raoul Luqué <raoulsluque@gmail.com>
2025-03-06 13:13:46 +0100

    Add option to add command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
+use clap::Parser;
 use rustboy::run;
 
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(name = "Rust Boy")]
+#[command(about = "A Game Boy emulator written in Rust", long_about = None)]
+struct Args {
+    /// If present, runs in game boy doctor mode
+    #[arg(short, long, default_value_t = false)]
+    doctor: bool,
+}
+
 fn main() {
-    pollster::block_on(run());
+    let args = Args::parse();
+
+    pollster::block_on(run(args.doctor));
 }
