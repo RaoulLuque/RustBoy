aea1e9b7badf3cc83f27d967c88dc18f38194826
Raoul Luqué <raoulsluque@gmail.com>
2025-03-13 17:29:30 +0100

    Move EI and DI instruction handling to separate file like with the other instructions

---
+++ src/cpu/instructions/di_and_ei.rs
@@ -0,0 +1,32 @@
+use crate::RustBoy;
+
+impl RustBoy {
+    /// Handles the di instruction
+    ///
+    /// Disables interrupts. Takes 1 cycle.
+    pub fn handle_di_instruction(&mut self) -> u16 {
+        self.di();
+        self.increment_cycle_counter(1);
+        self.pc.wrapping_add(1)
+    }
+
+    /// Sets the IME (Interrupt Master Enable) flag to false, which disables interrupts.
+    fn di(&mut self) {
+        self.ime = false;
+    }
+
+    /// Handles the ei instruction
+    ///
+    /// Enables interrupts. Takes 1 cycle.
+    pub fn handle_ei_instruction(&mut self) -> u16 {
+        self.ei();
+        self.increment_cycle_counter(1);
+        self.pc.wrapping_add(1)
+    }
+
+    /// Sets the ime_to_be_set flag to true, which will enable interrupts on the next instruction,
+    /// that is, will set the IME (Interrupt Master Enable) flag to true.
+    fn ei(&mut self) {
+        self.ime_to_be_set = true;
+    }
+}
