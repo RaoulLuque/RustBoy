2567f8cde1c246c7a6491480a188f3a8c61536e6
Raoul Luqué <raoulsluque@gmail.com>
2025-03-19 22:51:04 +0100

    Handle first scanline after LCD is turned back on again differently according to: https://www.reddit.com/r/EmuDev/comments/1cykjdr/gameboy_ppu_timing_question/

--- src/gpu.rs
+++ src/gpu.rs
@@ -43,6 +43,8 @@ pub struct GPU {
 pub struct RenderingInfo {
     pub(crate) dots_clock: u32,
     dots_for_transfer: u32,
+    lcd_was_turned_off: bool,
+    first_scanline_after_lcd_was_turned_on: bool,
 }
 
 /// Represents the possible rendering modes of the GPU.
@@ -88,36 +90,66 @@ impl GPU {
             self.gpu_registers
                 .set_ppu_mode(RenderingMode::VBlank1, interrupt_flags);
             self.gpu_registers.set_scanline(0, interrupt_flags);
+            self.rendering_info.lcd_was_turned_off = true;
             RenderTask::None
         } else {
-            self.rendering_info.dots_clock += cycles;
+            if self.rendering_info.lcd_was_turned_off {
+                // If the LCD was turned off, the GPU starts in HBlank mode and after this goes
+                // RenderingMode::Transfer3, which happens only after the lcd was turned on for the
+                // first "HBlank cycle", see:
+                // https://www.reddit.com/r/EmuDev/comments/1cykjdr/gameboy_ppu_timing_question/
+                // To make sure this irregularity is handled, we set the first_scanline_after_lcd_was_turned_on
+                // flag.
+                // TODO: Possibly handle that first frame after turning on the LCD is not actually
+                // sent to the screen, but rather just a blank screen.
+                self.gpu_registers
+                    .set_ppu_mode(RenderingMode::HBlank0, interrupt_flags);
+                self.rendering_info.first_scanline_after_lcd_was_turned_on = true;
+                self.rendering_info.lcd_was_turned_off = false;
+            }
+            self.rendering_info.dots_clock += dots;
             match self.gpu_registers.get_gpu_mode() {
                 RenderingMode::HBlank0 => {
-                    if self.rendering_info.dots_clock >= 456 - self.rendering_info.dots_for_transfer
-                    {
-                        self.rendering_info.dots_clock -=
-                            456 - self.rendering_info.dots_for_transfer;
-                        self.gpu_registers
-                            .set_scanline(self.gpu_registers.get_scanline() + 1, interrupt_flags);
-                        if self.gpu_registers.get_scanline() == 144 {
-                            // We are entering VBlank, so we need to set the VBlank flag
-                            // and set the GPU mode to VBlank. Also, we send a render frame request to
-                            // the GPU, which renders the framebuffer to the screen.
-                            self.gpu_registers.lcd_status.gpu_mode = RenderingMode::VBlank1;
+                    if self.rendering_info.first_scanline_after_lcd_was_turned_on {
+                        // If the LCD was turned off, it immediately enters HBlank mode which only
+                        // lasts 80 dots and then enters Transfer mode.
+                        if self.rendering_info.dots_clock >= 80 {
+                            self.rendering_info.dots_clock -= 80;
                             self.gpu_registers
-                                .set_ppu_mode(RenderingMode::VBlank1, interrupt_flags);
-                            interrupt_flags.vblank = true;
-                            return RenderTask::RenderFrame;
-                        } else {
-                            // We are still in HBlank, so we need to set the GPU mode to OAMScan2.
-                            // Also we send a request to the GPU to write the current line to the
-                            // framebuffer
-                            self.gpu_registers.lcd_status.gpu_mode = RenderingMode::OAMScan2;
-                            self.gpu_registers
-                                .set_ppu_mode(RenderingMode::OAMScan2, interrupt_flags);
-                            return RenderTask::WriteLineToBuffer(
-                                self.gpu_registers.get_scanline() - 1,
+                                .set_ppu_mode(RenderingMode::Transfer3, interrupt_flags);
+                            // We can now set the first_scanline_after_lcd_was_turned_on flag to
+                            // false, since after this we are in Transfer mode and then regular
+                            // HBlank mode, so the GPU can return to normal operation.
+                            self.rendering_info.first_scanline_after_lcd_was_turned_on = false;
+                        }
+                    } else {
+                        if self.rendering_info.dots_clock
+                            >= 456 - self.rendering_info.dots_for_transfer
+                        {
+                            self.rendering_info.dots_clock -=
+                                456 - self.rendering_info.dots_for_transfer;
+                            self.gpu_registers.set_scanline(
+                                self.gpu_registers.get_scanline() + 1,
+                                interrupt_flags,
                             );
+                            if self.gpu_registers.get_scanline() == 144 {
+                                // We are entering VBlank, so we need to set the VBlank flag
+                                // and set the GPU mode to VBlank. Also, we send a render frame request to
+                                // the GPU, which renders the framebuffer to the screen.
+                                self.gpu_registers
+                                    .set_ppu_mode(RenderingMode::VBlank1, interrupt_flags);
+                                interrupt_flags.vblank = true;
+                                return RenderTask::RenderFrame;
+                            } else {
+                                // We are still in HBlank, so we need to set the GPU mode to OAMScan2.
+                                // Also we send a request to the GPU to write the current line to the
+                                // framebuffer
+                                self.gpu_registers
+                                    .set_ppu_mode(RenderingMode::OAMScan2, interrupt_flags);
+                                return RenderTask::WriteLineToBuffer(
+                                    self.gpu_registers.get_scanline() - 1,
+                                );
+                            }
                         }
                     }
                 }
@@ -128,7 +160,6 @@ impl GPU {
                             .set_scanline(self.gpu_registers.get_scanline() + 1, interrupt_flags);
                         if self.gpu_registers.get_scanline() == 154 {
                             self.gpu_registers.set_scanline(0, interrupt_flags);
-                            self.gpu_registers.lcd_status.gpu_mode = RenderingMode::OAMScan2;
                             self.gpu_registers
                                 .set_ppu_mode(RenderingMode::OAMScan2, interrupt_flags);
                         }
@@ -137,7 +168,6 @@ impl GPU {
                 RenderingMode::OAMScan2 => {
                     if self.rendering_info.dots_clock >= 80 {
                         self.rendering_info.dots_clock -= 80;
-                        self.gpu_registers.lcd_status.gpu_mode = RenderingMode::Transfer3;
                         self.gpu_registers
                             .set_ppu_mode(RenderingMode::Transfer3, interrupt_flags);
                     }
@@ -147,7 +177,6 @@ impl GPU {
                     if self.rendering_info.dots_clock >= 172 {
                         self.rendering_info.dots_clock -= 172;
                         self.rendering_info.dots_for_transfer = 172;
-                        self.gpu_registers.lcd_status.gpu_mode = RenderingMode::HBlank0;
                         self.gpu_registers
                             .set_ppu_mode(RenderingMode::HBlank0, interrupt_flags);
                     }
@@ -202,6 +231,8 @@ impl GPU {
             rendering_info: RenderingInfo {
                 dots_clock: 0,
                 dots_for_transfer: 0,
+                lcd_was_turned_off: false,
+                first_scanline_after_lcd_was_turned_on: false,
             },
             gpu_registers: GPURegisters::new(debugging_flags),
             tile_data_changed: true,
